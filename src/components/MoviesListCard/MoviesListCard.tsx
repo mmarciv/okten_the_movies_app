073a06b1565c209cd8c1fa99c48246559ceeee8c
mmartsiv <mmartsiv@gmail.com>
2022-02-10 01:06:23 +0200

    Added styles for movies list

--- src/components/MoviesListCard/MoviesListCard.tsx
+++ src/components/MoviesListCard/MoviesListCard.tsx
@@ -4,21 +4,24 @@ import MovieInfo from "../MovieInfo/MovieInfo";
 // @ts-ignore
 import StarRatings from 'react-star-ratings';
 import "./MoviesListCard.css"
+import {NavLink} from "react-router-dom";
 
 const MoviesListCard: any = ({card}: any) => {
     return (
         <div className={'card'}>
-            <PosterPreview poster_path={card.poster_path}/>
-            <MovieInfo card={card}/>
-            <div className={'card__rating'}>
-                <StarRatings key={card.id}
-                             rating={3}
-                             starRatedColor="black"
-                             numberOfStars={5}
-                             starDimension="30px"
-                             starSpacing="10px"
-                             name='rating'/>
-            </div>
+                <PosterPreview poster_path={card.poster_path}/>
+                <NavLink to={'/movie/' + card.id}>
+                    <MovieInfo card={card}/>
+                </NavLink>
+                <div className={'card__rating'}>
+                    <StarRatings key={card.id}
+                                 rating={3}
+                                 starRatedColor="black"
+                                 numberOfStars={5}
+                                 starDimension="30px"
+                                 starSpacing="10px"
+                                 name='rating'/>
+                </div>
         </div>
     );
 };
