c6c5b344c9d2036a75e49693131e838c9e8e9b85
mmartsiv <mmartsiv@gmail.com>
2022-02-11 00:13:33 +0200

    Added styles for movies list

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -3,6 +3,11 @@ import MoviesListCard from "../components/MoviesListCard/MoviesListCard";
 import {useDispatch, useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
 import {getMovieDetails, getMoviesByGenre, getPopularMovie} from "../store/movie.slice";
+import {mediaURL} from "../configs/urls";
+
+import "./MovieDetailsPage.css"
+import {IMovie} from "../models/Movie";
+import {IGenre} from "../models/Genre";
 
 const MoviesDetailsPage = () => {
 
@@ -21,8 +26,28 @@ const MoviesDetailsPage = () => {
     }, [movieId]);
 
     return (
-        <div className={''}>
-            {movie && <MoviesListCard card={movie}/>}
+        <div>
+            {movie &&
+                <div className={'page'}>
+                    <div className={'page__img'}>
+                        <img src={mediaURL + movie.poster_path} alt="" width="400"/>
+                    </div>
+                    <div className={'page__movie__info'}>
+                        <div className={''}>
+                            <h2>{movie.title}</h2>
+                            <div>{movie.overview}</div>
+                            <h3>Genres</h3>
+                            <div>
+                                {movie.genres && movie.genres.map((ganre: IGenre) => {
+                                    return <div>{ganre.name}</div>
+                                })}
+                            </div>
+                            <h3>Vote range</h3>
+                            <div>{movie.vote_average}</div>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     );
 };
